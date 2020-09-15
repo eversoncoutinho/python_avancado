f9f876b50cd729046bef0963f27531374030c150
eversoncoutinho <eversoncoutinho@gmail.com>
2020-09-14 22:41:12 -0300

    comit

--- python_avancado/site_/urls.py
+++ python_avancado/site_/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('urlpai/', include('blog.urls')), #a url pai
-    
 ]
